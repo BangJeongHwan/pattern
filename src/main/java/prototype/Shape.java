e3da6e3355f5b1ac574bfd8cb555886ea97a8b23
BangJeongHwan <baju92@humuson.com>
2021-09-20 20:28:04 +0900

    프로토 타입 패턴 1

---
+++ src/main/java/prototype/Shape.java
@@ -0,0 +1,16 @@
+package prototype;
+
+public class Shape implements Cloneable{
+
+    private String id;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
+
+
