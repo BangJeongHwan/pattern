c16933147aa3ddcd5ab1d78809bce1bc036ac912
BangJeongHwan <baju92@humuson.com>
2021-09-22 15:24:11 +0900

    빌더 패턴 2

---
+++ src/main/java/builder02/Main.java
@@ -0,0 +1,15 @@
+package builder02;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = ComputerBuilder
+//                .start()
+//                .setCpu("i7")
+                .startWithCpu("i7")
+                .setRam("8g")
+                .setStorage("256g ssd")
+                .build();
+
+        System.out.println(computer.toString());
+    }
+}
