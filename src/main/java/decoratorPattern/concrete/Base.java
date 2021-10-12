2e252251e88fb3b882b373455d6c38a9c2320d04
BangJeongHwan <baju92@humuson.com>
2021-10-12 16:07:39 +0900

    데코레이터 패턴

---
+++ src/main/java/decoratorPattern/concrete/Base.java
@@ -0,0 +1,19 @@
+package decoratorPattern.concrete;
+
+import decoratorPattern.abst.IBeverage;
+
+/**
+ * <h2><b>Role : </b>ConcreteComponent</h2>
+ *  <p>
+ *  Component의 실질적인 인스턴스의 부분이며, 책임의 주체입니다.
+ *  </p>
+ * @author garam park
+ *
+ */
+public class Base implements IBeverage {
+
+    public int getTotalPrice() {
+        return 0;
+    }
+
+}
