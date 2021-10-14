3462e4869e3ba9d32f0855f2e935735b57673cc6
BangJeongHwan <baju92@humuson.com>
2021-10-14 23:44:20 +0900

    옵저버 패턴 1

---
+++ src/main/java/observerPattern01/Application.java
@@ -0,0 +1,14 @@
+package observerPattern01;
+
+public class Application {
+    public static void main(String[] args) {
+        Button button = new Button();
+
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(Button button) {
+                System.out.println(button+" is Clicked");
+            }
+        });
+        button.onClick();
+    }
+}
